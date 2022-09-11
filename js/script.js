020063fef4c825266b6238c1bfa56c5e54c91028
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-11 01:25:24 +0100

    draw lines

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let isDrawing = false;
+
+window.addEventListener('load', () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+});
+
+const drawing = (e) => {
+    if (!isDrawing) return;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+}
+
+const startDrawing = (e) => {
+    isDrawing = true;
+    ctx.beginPath();
+}
+
+const stopDrawing = (e) => {
+    isDrawing = false;
+}
+
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mousemove', drawing);
