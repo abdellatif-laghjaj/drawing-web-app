ee892a528ab85cc85012e1fce053b97ffc345633
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-11 01:34:11 +0100

    update UI

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById('canvas');
+const toolBtns = document.querySelectorAll('.tool-btn');
 const ctx = canvas.getContext('2d');
 
+console.log(toolBtns);
+
 let isDrawing = false;
+let brushWidth = 5;
 
 window.addEventListener('load', () => {
     canvas.width = canvas.offsetWidth;
@@ -17,11 +21,18 @@ const drawing = (e) => {
 const startDrawing = (e) => {
     isDrawing = true;
     ctx.beginPath();
+    ctx.lineWidth = brushWidth;
 }
 
 const stopDrawing = (e) => {
     isDrawing = false;
 }
+
+toolBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn.id);
+    });
+});
 
 canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mouseup', stopDrawing);
